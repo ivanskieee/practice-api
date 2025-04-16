2038ff918fee294d0cab1ed5a55ceaba5b2192c0
Ivan Brilata <brilataivan86@gmail.com>
2025-04-16 14:21:23 +0800

    first commit

---
+++ app/controllers/api/posts_controller.rb
@@ -0,0 +1,47 @@
+class Api::PostsController < ApplicationController
+  before_action :set_post, only: [:show, :update, :destroy]
+
+  # GET /api/posts
+  def index
+    @posts = Post.all
+    render json: @posts
+  end
+
+  # GET /api/posts/:id
+  def show
+    render json: @post
+  end
+
+  # POST /api/posts
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      render json: @post, status: :created
+    else
+      render json: @post.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /api/posts/:id
+  def update
+    if @post.update(post_params)
+      render json: @post
+    else
+      render json: @post.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /api/posts/:id
+  def destroy
+    @post.destroy
+  end
+
+  private
+    def set_post
+      @post = Post.find(params[:id])
+    end
+
+    def post_params
+      params.require(:post).permit(:title, :body)
+    end
+end
